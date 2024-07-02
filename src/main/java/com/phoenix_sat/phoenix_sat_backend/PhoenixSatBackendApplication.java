e3b5c73ebe3be794bb91a4bcbf442c4382379ccd
nicatsemedov777 <smdovn42@gmail.com>
2024-07-02 19:21:17 +0400

    first commit

---
+++ src/main/java/com/phoenix_sat/phoenix_sat_backend/PhoenixSatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix_sat.phoenix_sat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixSatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixSatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenix_sat/phoenix_sat_backend/PhoenixSatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix_sat.phoenix_sat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixSatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
